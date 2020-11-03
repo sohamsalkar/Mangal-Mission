da406846bcaf73b95ea007c62a110b4e9ce2a63f
Soham Salkar <salkarsoham2@gmail.com>
2020-11-03 13:36:49 +0530

    MMApp

---
+++ Mangal MissionTests/Mangal_MissionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mangal_MissionTests.swift
+//  Mangal MissionTests
+//
+//  Created by Soham Salkar on 31/10/20.
+//
+
+import XCTest
+@testable import Mangal_Mission
+
+class Mangal_MissionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
